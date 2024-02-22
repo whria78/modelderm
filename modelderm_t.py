b80f34ae93838bd927e2856ae8d650e52cae0fda
whria78 <whria78@gmail.com>
2024-02-22 22:18:32 +0900

    initial commit

--- modelderm_t.py
+++ modelderm_t.py
@@ -2,19 +2,8 @@
 ### Model Dermatology (http://t.modelderm.com) REST API
 ### Han Seung Seog (whria78@gmail.com) 
 ### 
-### 2020-12-6
+### 2024-2-22
 ###
-
-
-###### Example of Result 
-###
-### Error : <error>0</error> 
-### Metadata converted : <race>1</race> <sex>1</sex> <age>70</age> <loc>arm</loc> <pruritus>o</pruritus> <pain>o</pain> <onset>w</onset> 
-### Metadata used or not : <meta_used>1</meta_used> 
-### Number of photos analyzed : <no_photo>1</no_photo> 
-### Top outputs : <disease>Insect bite,0.2728,Melanocytic nevus,0.2456,Xerotic eczema,0.0761,Hemangioma,0.0704,Folliculitis,0.0561,Cherry Hemangioma,0.0379,Skin tag,0.0357,Dysplastic nevus,0.0356,Inflammed cyst,0.0240,Nonspecific (normal),0.0157</disease> 
-### Malignancy output : <malignancy_output>4</malignancy_output>
-
  
 import os
 import requests
@@ -51,12 +40,7 @@ def get_server_status(url,api_key):
 
 def modelderm(url,api_key,image_path,imencoded,meta_):
     args1_=api_key # API KEY
-    if meta_ is None:
-        args2_='<id></id><race></race><birth></birth><sex></sex><location></location><pruritus></pruritus><pain></pain><onset></onset>'
-    else:
-        age_,sex_=meta_
-        args2_='<birth>%d</birth><sex>%s</sex>' % (age_,sex_)
-   
+    args2_='<id></id><race></race><birth></birth><sex></sex><location></location><pruritus></pruritus><pain></pain><onset></onset>'
     
     file_=[]
     file_+=[('file',(custom_filename(os.path.basename(image_path)),imencoded,'Content-Type:image'))]
@@ -67,14 +51,12 @@ def modelderm(url,api_key,image_path,imencoded,meta_):
 
     return res.content
 
+###
 ### Server List ### 
+###
 server_url='https://t.modelderm.com/api'
-
-
-###
-### GENERATE UNIQUE ID FOR CHECKING STATUS
-###
 api_key="test"
+
 
 ###
 ### GET SERVER STATUS
@@ -93,9 +75,8 @@ for root,dirs,files in os.walk(img_root):
             img_list+=[img_path.replace('\\','/')]
 
 
-use_meta=False
 f=open('log.txt','a')
-f.write('START / USE_META %d / %s\n' % (use_meta,datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+f.write('START %s\n' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 for processed_,img_path in enumerate(img_list):
     print(img_path,processed_,len(img_list))
     img_org=cv2.imread(img_path,cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
@@ -122,24 +103,9 @@ for processed_,img_path in enumerate(img_list):
     display_size=1500
     display_img=cv2.resize(crop_img,(display_size,display_size))
     
-    if use_meta:
-        meta_info=re.split('_|-',os.path.splitext(os.path.basename(img_path))[0])
-        sex_=meta_info[1].lower()
-        birth_=datetime.today().year-int(meta_info[2])
-        meta_=[birth_,sex_]
-
-        result_raw=modelderm(server_url,api_key,img_path,imencoded,meta_).decode('unicode_escape')
-    else:
-        birth_=-1
-        sex_=''
-
-        result_raw=modelderm(server_url,api_key,img_path,imencoded,None).decode('unicode_escape')
+    result_raw=modelderm(server_url,api_key,img_path,imencoded,None).decode('unicode_escape')
     print(result_raw) 
 
 f.write('END / %s\n' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 print("FINISHED")    
-
-
-# Please check the format of filename (sex/age)
-# Please be sure to use rectangle image as an input image.
